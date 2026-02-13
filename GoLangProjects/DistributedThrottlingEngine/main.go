41f792c53bcb711afeb44234829480981fbb3b31
KUMAR-SHREYAS <yashsahay00@gmail.com>
2026-02-13 14:59:08 +0530

    add: homePage() and loginPage(), implement: middleware

--- GoLangProjects/DistributedThrottlingEngine/main.go
+++ GoLangProjects/DistributedThrottlingEngine/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"net"
 	"net/http"
 	"time"
 
@@ -10,34 +11,43 @@ import (
 )
 
 var ctx = context.Background()
+var rdb *redis.Client
 
-func main() {
-	// 1. Establishing connection to local redis server using rdb client.
-	rdb := redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
-	})
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path == "/favicon.ico" { // Disable the tab icon request.
+func rateLimitMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/favicon.ico" {
 			return
 		}
-		userIp := r.RemoteAddr //  Identifying the visitor with IP Address
-
-		count, err := rdb.Incr(ctx, userIp).Result() // redis INCR counts the userIp hits
+		userIp, _, err := net.SplitHostPort(r.RemoteAddr)
 		if err != nil {
-			http.Error(w, "Redis Error", 500)
-			return
+			userIp = r.RemoteAddr
 		}
+		count, _ := rdb.Incr(ctx, userIp).Result()
 
-		if count == 1 { // first count sets an expiration to the counter
+		if count == 1 {
 			rdb.Expire(ctx, userIp, 10*time.Second)
 		}
-
-		if count > 5 {// rate limiter
-			fmt.Fprintf(w, "Slow down!, You've visited %d times in 10 seconds. Blocked!", count)
+		if count > 5 {
+			http.Error(w, "Too many requests. Slow down!", http.StatusTooManyRequests)
 			return
 		}
-		fmt.Fprintf(w, "Welcome! You have visited %d times. Refresh to test the limit.", count)
+		next(w, r)
+	}
+}
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HOME page!.")
+}
+func loginPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the LOGIN page!.")
+}
+func main() {
+	// 1. Establishing connection to local redis server using rdb client.
+	rdb = redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
 	})
+
+	http.HandleFunc("/", rateLimitMiddleware(homePage))
+	http.HandleFunc("/login", rateLimitMiddleware(loginPage))
 
 	fmt.Println("Server starting at :8080...")
 	http.ListenAndServe(":8080", nil) // spin up the server.
